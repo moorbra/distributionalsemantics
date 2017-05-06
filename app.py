0dd9b540176d9d5b7ce9f1739a6bb46a3cdb80d9
Brandon Moore <moorbra44@hotmail.com>
2017-05-06 08:53:44 -0500

    Added flask templates

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 """Semantics Api"""
-from flask import Flask
+from flask import Flask, url_for, render_template
 from flask_restful import Api
 from resources.wikipediacategory import WikipediaCategory
 
 app = Flask(__name__)
 api = Api(app)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 api.add_resource(WikipediaCategory, '/semantics/categories', endpoint='categories')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80, debug=True)
--- resources/wikipediacategory.py
+++ resources/wikipediacategory.py
@@ -1,9 +1,15 @@
 """Wikipedia microservice. """
 from flask_restful import Resource
+import requests
 
 class WikipediaCategory(Resource):
     """Microservice for working with Wikipedia article catagories"""
+
+    WIKI_API_BASE_URL = "https://en.wikipedia.org/w/api.php"
+    WIKI_USER_AGENT = {'user-agent': 'mooresemantics/1.0.0' }
+
     def get(self):
+        #r = requests.get(url, headers=headers)
         return { 'Hello': 'World' }
 
     def post(self):
